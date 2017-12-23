fc78ac60042e7ce575e8157d28f952de7fab811f
docioban <34736746+docioban@users.noreply.github.com>
2017-12-23 12:46:58 +0200

    Add files via upload

---
+++ get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: docioban <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/18 07:04:30 by docioban          #+#    #+#             */
+/*   Updated: 2017/12/23 11:54:58 by docioban         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include "libft/libft.h"
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+# define BUFF_SIZE 32
+
+int		get_next_line(int const fd, char **line);
+
+#endif
